86eddd17c8b86e0962b6b0ab1bbbe534c4f8eda0
Matej <matej@occupationbliss.com>
2025-02-26 09:28:56 +0100

    Create an HTTP server. WOW

---
+++ 07_museum/api/post.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"frontendmasters.com/go/museum/data"
+)
+
+func Post(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodPost {
+        var exhibition data.Exhibition
+        err := json.NewDecoder(r.Body).Decode(&exhibition)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusBadRequest)
+            return
+        }
+        data.Add(exhibition)
+        w.Write([]byte("OK"))
+    } else {
+        http.Error(w, "Unsupported Method", http.StatusMethodNotAllowed)
+    }
+}
